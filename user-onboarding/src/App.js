30e2d343d205dfe4bcaf5d070c1eb35f24e306ba
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-19 15:13:16 -0500

    working on app structure

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import UserCard from './UserCard';
+import Form from './Form';
+import axios from 'axios';
+import * as yup from 'yup';
+import schema from '../validation/formSchema'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  termsOfService: false
+};
 
-export default App;
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  termsOfService: false
+};
+
+const initialUsers = [];
+const initialDisabled = true;
+
+export default function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        setUsers(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        setUsers([res.data, ...users])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      });
+  };
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  };
+
+
+
+
+
+};
