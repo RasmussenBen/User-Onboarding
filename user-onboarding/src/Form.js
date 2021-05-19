08d2ce54cd348f5afa1de059b2b3517440bfc345
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-19 16:15:04 -0500

    working through form setup

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,2 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
+export default function UserForm(props) {
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors
+    } = props
+
+    const onSubmit = event => {
+        event.preventDefault()
+        submit()
+    }
+
+    const onChange = event => {
+        const { name, value, checked, type } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        change(name, valueToUse)
+    }
+
+    return (
+        <form className = 'form container' onSubmit = {onSubmit}>
+            <div className = 'form-group submit'>
+                <h2>Add a User</h2>
+
+                <button disabled = {disabled}>Submit</button>
+
+                <div className = 'errors'>
+                    <div>{errors.name</div>
+                    <div>{errors.email</div>
+                    <div>{errors.password</div>
+                    // <div>{errors.termsOfService</div>
+                </div>
+            </div>
+            
+            <div className = 'form-group inputs'>
+                
+            </div>
+        </form>
+    )
+
+
+}
